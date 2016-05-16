c5a0751de562b88683c63e3b76e3db84b2c86f82
Robert Haken <haken@havit.cz>
2016-05-16 21:16:49 +0200

    WebApplicationTemplate.Tests + Dummy MSTest

---
+++ AspNetCoreDependencyInjectionDemos/test/WebApplicationTemplate.Tests/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplicationTemplate.Tests
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			//Dummy Main method so DNX stops complaining.
+		}
+	}
+}
---
+++ AspNetCoreDependencyInjectionDemos/test/WebApplicationTemplate.Tests/Services/Sales/PriceResolverTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebApplicationTemplate.Tests.Services.Sales
+{
+	[TestClass]
+	public class PriceResolverTests
+	{
+		[TestMethod]
+		public void PriceResolver_BasicPriceOnly_ReturnsBasicPrice()
+		{
+			// arrange
+
+			// act
+
+			// assert
+			Assert.Fail("test");
+		}
+	}
+}
